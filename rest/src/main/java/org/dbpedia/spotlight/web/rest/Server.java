d61220ee1a49ca6e36e3ee19d3ffcd805bbbf31b
Roland <roland.jungnickel@idioplatform.com>
2013-09-13 10:33:39 +0100

    enable server  monitoring

--- rest/src/main/java/org/dbpedia/spotlight/web/rest/Server.java
+++ rest/src/main/java/org/dbpedia/spotlight/web/rest/Server.java
@@ -111,6 +111,7 @@ public class Server {
 
 
         SelectorThread threadSelector = GrizzlyWebContainerFactory.create(serverURI, initParams);
+        threadSelector.enableMonitoring();
         threadSelector.start();
 
         System.err.println("Server started in " + System.getProperty("user.dir") + " listening on " + serverURI);
